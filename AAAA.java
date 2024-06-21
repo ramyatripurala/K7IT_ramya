fb8ef7c10d183896e4afb4f51e1cb1f3f46f665f
Tripurala Tharun <tripuralatharun>
2024-06-21 11:03:17 +0530

    okk

---
+++ AAAA.java
@@ -0,0 +1,40 @@
+class Raghavagaru{
+int age;
+String name;
+double height;
+double weight;
+
+public static void main (String args[]){
+Raghavagaru r1 = new Raghavagaru();
+r1.age = 27;
+r1.name = "Sita";
+r1.height = 5.9;
+r1.weight = 65.5;
+
+Raghavagaru r2 = new Raghavagaru();
+r2.age = 25;
+r2.name = "varun";
+r2.height = 2.25;
+r2.weight = 25.0;
+
+System.out.println("Raghavagaru  details");
+System.out.println("name:"+ r1.name);
+System.out.println("age:"+ r1.age);
+System.out.println("height:"+ r1.height);
+System.out.println("weight:"+ r1.weight); 
+
+
+System.out.println("Raghavagaru details");
+System.out.println("name:"+ r2.name);
+System.out.println("age:"+ r2.age);
+System.out.println("height:"+ r2.height);
+System.out.println("weight:"+ r1.weight);
+
+
+
+
+
+
+
+}
+}
